316b54d32937ba5ef1a6ca2ed86ef4f6f06ad7eb
nayana005 <nayanasuresh1005@gmail.com>
2022-06-10 00:43:59 +0530

    Added LinkedList by Add

---
+++ LinkList1.java
@@ -0,0 +1,15 @@
+package com.bl.linkedList;
+
+public class LinkList1 {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Linked List Program");
+		LinkedList list=new LinkedList();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		list.display();
+
+	}
+}
+
+
